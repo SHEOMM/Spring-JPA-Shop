d4822453b51a5896d0cfb195e042355b13f39a50
SHEOMM <esh0821@snu.ac.kr>
2024-01-30 21:49:55 +0900

    init commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+
+
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        // 저장과 조회가 같을까? 같다. 영속성 컨텍스트
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
